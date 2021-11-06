9f580c0fd545857a0fee7d7e0d9f3d3d6c4369d0
Yudo Kartiko <yudoworks@gmail.com>
2021-11-07 01:45:40 +0700

    Revert "check test image for profile image"
    This reverts commit 2acdc073adbe7d4ea2fa0325d9e41ea65a2841af.

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,7 +2,7 @@ import { Link } from 'gatsby';
 import get from 'lodash/get';
 import React from 'react';
 
-import profileImg from '../../images/test.jpg';
+import profileImg from '../../images/profile.jpg';
 
 const classes = {
   wrapper: 'block mb-6 md:flex',
